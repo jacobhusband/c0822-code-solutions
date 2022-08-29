1a4fcf07f5c4398db3bad1dfcddd326c3e95e959
Jacob Husband <jakehusband2@gmail.com>
2022-08-29 16:25:20 +0000

    Completed javascript-primitives-and-variables exercises

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,17 @@
+const fullName = 'Jacob Husband';
+const isCool = true;
+const totalPets = 0;
+const unknown = null;
+let never;
+
+console.log('Full name: ', fullName);
+console.log('Am I cool? ', isCool);
+console.log('How many pets do I have? ', totalPets);
+
+console.log('typeof fullName: ', typeof fullName);
+console.log('typeof isCool: ', typeof isCool);
+console.log('typeof totalPets: ', typeof totalPets);
+console.log('value of unknown: ', unknown);
+console.log('typeof unknown', typeof unknown);
+console.log('value of never: ', never);
+console.log('typeof never', typeof never);
