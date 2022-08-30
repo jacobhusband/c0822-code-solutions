531406ac73bcb12bb67b4fb64ceab0865c363c1a
Jacob Husband <jakehusband2@gmail.com>
2022-08-30 16:42:15 +0000

    Completed javascript-data-model exercises

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,70 @@
+var orderHistory = [
+  {
+    items: [
+      {
+        product: "JavaScript for impatient programmers",
+        item: 1
+        owner: "Rauschmayer, Dr. Axel",
+        initial_price: 31.55,
+        return_window_date: 20200907,
+      },
+    ],
+    total_price: 34.00,
+    id: "114-3941689-8772232",
+    order_date: 20200804,
+    buyer: "JS Masher",
+  },
+  {
+    items: [
+      {
+        product: "The Timeless Way of Building",
+        item: 1,
+        owner: "Alexander, Christopher",
+        initial_price: 41.33,
+        return_window_date: 20200819,
+      },
+    ],
+    total_price: 44.53,
+    id: "113-9984268-1280257",
+    order_date: 20200719,
+    buyer: "JS Masher",
+  },
+  {
+    items: [
+      {
+        product:
+      "Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter",
+        item: 1,
+        owner: null,
+        initial_price: 15.98,
+        return_window_date: 20200805,
+      },
+    ],
+    total_price: 17.22,
+    id: "114-2875557-9059409",
+    order_date: 20200704,
+    buyer: "JS Masher",
+  },
+  {
+    items: [
+      {
+        product: "GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)",
+        item: 1,
+        owner: null,
+        initial_price: 94.95,
+        return_window_date: 20200804,
+      },
+      {
+        product: "The Art of Sql",
+        item: 2,
+        owner: "Faroult, Stephane",
+        initial_price: 33.99,
+        return_window_date: 20200804,
+      }
+    ]
+    total_price: 138.93,
+    id: "113-2883177-2648248",
+    order_date: 20200703,
+    buyer: "JS Masher",
+  },
+];
