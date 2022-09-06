b6ad61ffecf1c6e3a23197c54395d414f951acf3
Jacob Husband <jakehusband2@gmail.com>
2022-09-06 23:07:44 +0000

    Remove logic that is dependent on current state of DOM

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,14 +1,17 @@
 var $room = document.querySelector('.container');
 var $bulb = $room.firstElementChild;
+var lightOn = false;
 
 $bulb.addEventListener('click', brightenRoom);
 
 function brightenRoom(event) {
-  if ($room.className === 'container dim') {
+  if (!lightOn) {
     $room.className = 'container bright';
     $bulb.className = 'bulb light';
+    lightOn = true;
   } else {
     $room.className = 'container dim';
     $bulb.className = 'bulb dark';
+    lightOn = false;
   }
 }
