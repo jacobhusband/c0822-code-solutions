940a9a58fdfe148609d8ef7ae208450675c33eae
Jacob Husband <jakehusband2@gmail.com>
2022-09-07 19:03:21 +0000

    Change complex card creation and change block of code to loop

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -3,48 +3,32 @@ console.log('Lodash is loaded:', typeof _ !== 'undefined');
 var playerArray = [];
 var cardDeck = [];
 
+var possibleRanks = [
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  'ace',
+  'jack',
+  'queen',
+  'king'
+];
+var possibleSuits = ['clubs', 'diamonds', 'hearts', 'spades'];
+
 // Make players
 for (var n = 0; n < 4; n++) {
   playerArray.push({ name: n + 1, hand: [] });
 }
 
 // Make deck
-for (var i = 1; i < 10; i++) {
-  for (var j = 0; j < 4; j++) {
-    if (j === 0) {
-      cardDeck.push({ rank: i + 1, suit: 'clubs' });
-    } else if (j === 1) {
-      cardDeck.push({ rank: i + 1, suit: 'diamonds' });
-    } else if (j === 2) {
-      cardDeck.push({ rank: i + 1, suit: 'hearts' });
-    } else if (j === 3) {
-      cardDeck.push({ rank: i + 1, suit: 'spades' });
-    }
-  }
-}
-
-var rank;
-
-for (var k = 0; k < 4; k++) {
-  if (k === 0) {
-    rank = 'ace';
-  } else if (k === 1) {
-    rank = 'jack';
-  } else if (k === 2) {
-    rank = 'queen';
-  } else if (k === 3) {
-    rank = 'king';
-  }
-  for (var l = 0; l < 4; l++) {
-    if (l === 0) {
-      cardDeck.push({ rank, suit: 'clubs' });
-    } else if (l === 1) {
-      cardDeck.push({ rank, suit: 'diamonds' });
-    } else if (l === 2) {
-      cardDeck.push({ rank, suit: 'hearts' });
-    } else if (l === 3) {
-      cardDeck.push({ rank, suit: 'spades' });
-    }
+for (var i = 0; i < possibleRanks.length; i++) {
+  for (var j = 0; j < possibleSuits.length; j++) {
+    cardDeck.push({ rank: possibleRanks[i], suit: possibleSuits[j] });
   }
 }
 
@@ -60,15 +44,13 @@ for (var m = 51; m >= 0; m--) {
 
 console.log('Shuffled Deck: ', shuffledDeck);
 
-// Deal cards
-playerArray[0].hand.push(shuffledDeck[0]);
-playerArray[0].hand.push(shuffledDeck[1]);
-playerArray[1].hand.push(shuffledDeck[2]);
-playerArray[1].hand.push(shuffledDeck[3]);
-playerArray[2].hand.push(shuffledDeck[4]);
-playerArray[2].hand.push(shuffledDeck[5]);
-playerArray[3].hand.push(shuffledDeck[6]);
-playerArray[3].hand.push(shuffledDeck[7]);
+var cardDealingCount = 0;
+for (var ind = 0; ind < 8; ind++) {
+  playerArray[cardDealingCount].hand.push(shuffledDeck[ind]);
+  if (ind % 2 === 1) {
+    cardDealingCount++;
+  }
+}
 
 // Find highest score player
 var maxIndex;
