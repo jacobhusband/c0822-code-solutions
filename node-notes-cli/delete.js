55638b9aadc92adbfbd2940f097836aa518de3bd
Jacob Husband <jakehusband2@gmail.com>
2022-10-12 23:42:59 +0000

    Remove nextId decrement from delete module

--- node-notes-cli/delete.js
+++ node-notes-cli/delete.js
@@ -6,18 +6,11 @@ const remove = userKey => {
       process.exit(1);
     }
     data = JSON.parse(data);
-    const arr = [];
-    const obj = {};
     for (const key in data.notes) {
-      if (userKey !== key) {
-        arr.push(data.notes[key]);
+      if (userKey === key) {
+        delete data.notes[key];
       }
     }
-    arr.forEach((item, ind) => {
-      obj[ind + 1] = item;
-    });
-    data.notes = obj;
-    data.nextId--;
     data = JSON.stringify(data, null, 2);
     fs.writeFile('data.json', data, 'utf8', err => {
       if (err) throw err;
