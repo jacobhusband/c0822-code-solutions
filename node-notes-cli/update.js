b011fd6d1af3fb5efb02e226df2f00982df41f36
Jacob Husband <jakehusband2@gmail.com>
2022-10-12 23:48:39 +0000

    Stop update from occuring if user tries to update an entry with an id greater than or equal to data.nextId

--- node-notes-cli/update.js
+++ node-notes-cli/update.js
@@ -6,16 +6,22 @@ const update = (userKey, input) => {
       process.exit(1);
     }
     data = JSON.parse(data);
-    for (var key in data.notes) {
-      if (userKey === key) {
-        data.notes[key] = input;
-        break;
+    if (parseInt(userKey) < parseInt(data.nextId)) {
+      for (var key in data.notes) {
+        if (userKey === key) {
+          data.notes[key] = input;
+          break;
+        }
       }
+      data = JSON.stringify(data, null, 2);
+      fs.writeFile('data.json', data, 'utf8', err => {
+        if (err) throw err;
+      });
+    } else {
+      console.log(
+        'This id has never existed, choose an id that has existed or create a new entry!'
+      );
     }
-    data = JSON.stringify(data, null, 2);
-    fs.writeFile('data.json', data, 'utf8', err => {
-      if (err) throw err;
-    });
   });
 };
 
