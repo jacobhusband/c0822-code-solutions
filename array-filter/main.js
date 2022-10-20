17951bb58626d09560177710fb5e47af4a885c0a
Jacob Husband <jakehusband2@gmail.com>
2022-10-20 16:12:15 +0000

    Completed array.filter exercises

---
+++ array-filter/main.js
@@ -0,0 +1,18 @@
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const names = [
+  'Ada',
+  'Hedy',
+  'Jean',
+  'Grace',
+  'Evelyn',
+  'Joan',
+  'Elizabeth',
+  'Janese',
+  'Donna'
+];
+
+const evenNumbers = numbers.filter(el => !(el % 2));
+const overFive = numbers.filter(el => el > 5);
+const startWithE = names.filter(el => el[0] === 'E');
+const haveD = names.filter(el => el.includes('d'));
