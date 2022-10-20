66f395f1444794763fb4a89983a0b9f169282e94
Jacob Husband <jakehusband2@gmail.com>
2022-10-20 16:31:47 +0000

    Create filter function

--- array-filter/main.js
+++ array-filter/main.js
@@ -16,3 +16,15 @@ const evenNumbers = numbers.filter(el => !(el % 2));
 const overFive = numbers.filter(el => el > 5);
 const startWithE = names.filter(el => el[0] === 'E');
 const haveD = names.filter(el => el.includes('d') || el.includes('D'));
+
+const checkFilter = filter(numbers, el => !(el % 2));
+
+function filter(array, cb) {
+  const output = [];
+  array.forEach(item => {
+    if (cb(item)) {
+      output.push(item);
+    }
+  });
+  return output;
+}
