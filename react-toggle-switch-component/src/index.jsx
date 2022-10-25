e61cd3f161f6bf1a99e069a7050cbb1c4cd516c5
Jacob Husband <jakehusband2@gmail.com>
2022-10-25 18:59:07 +0000

    Completed react-toggle-switch-component exercises

---
+++ react-toggle-switch-component/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ToggleSwitch from './toggle-switch';
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+
+root.render(<ToggleSwitch/>);
