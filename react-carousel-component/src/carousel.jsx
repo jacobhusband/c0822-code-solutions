2085d155c17f2db3f9e016519e6b9825b091767a
Jacob Husband <jakehusband2@gmail.com>
2022-10-27 04:02:51 +0000

    Finish basic layout with first image and first dot colored

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -1,11 +1,73 @@
 import React from 'react';
 
 export default class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentId: 1
+    };
+  }
+
   render() {
     return (
-      <div>
-        Hi
+      <div className='container'>
+        <div className='row content'>
+          <div className='col flex-col self-center'>
+            <i className="fa-solid fa-chevron-left"></i>
+          </div>
+          <div className='col flex-col'>
+            <Pokemon images={this.props.images} currentId={this.state.currentId}/>
+            <Dots images={this.props.images} currentId={this.state.currentId}/>
+          </div>
+          <div className='col flex-col self-center'>
+            <i className="fa-solid fa-chevron-right"></i>
+          </div>
+        </div>
       </div>
+    );
+  }
+}
+
+class Pokemon extends React.Component {
+
+  render() {
+    const listItems = this.props.images.map(obj => {
+      const item = (
+        <li key={obj.id}>
+          <img src={obj.url} alt={obj.name} />
+        </li>
+      );
+      if (Number(item.key) === this.props.currentId) {
+        return item;
+      }
+      return null;
+    });
+
+    return (
+      <ul>
+        {listItems}
+      </ul>
+    );
+  }
+}
+
+class Dots extends React.Component {
+
+  render() {
+    const listItems = this.props.images.map(image => {
+      let item;
+      if (Number(image.id) === this.props.currentId) {
+        item = <li key={image.id} className='dot dark-bg'></li>;
+      } else {
+        item = <li key={image.id} className='dot'></li>;
+      }
+      return item;
+    });
+
+    return (
+      <ul className="dots self-center">
+        {listItems}
+      </ul>
     );
   }
 }
--- react-carousel-component/src/index.jsx
+++ react-carousel-component/src/index.jsx
@@ -4,4 +4,22 @@ import Carousel from './carousel';
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 
-root.render(<Carousel/>);
+const images = [
+  {
+    name: 'pikachu',
+    url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png',
+    id: 1
+  },
+  {
+    name: 'squirtle',
+    url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png',
+    id: 2
+  },
+  {
+    name: 'charmander',
+    url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
+    id: 3
+  }
+];
+
+root.render(<Carousel images={images}/>);
