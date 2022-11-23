29efc75ad1818768a0a0b704856f758a07b9c2c0
Jacob Husband <jakehusband2@gmail.com>
2022-11-23 22:39:45 +0000

    Completed javascript-closures exercises

---
+++ javascript-closures/main.js
@@ -0,0 +1,11 @@
+function graduate(credential) {
+  return function (fullName) {
+    return `${fullName}, ${credential}`;
+  };
+}
+
+const medicalSchool = graduate('M.D.');
+const lawSchool = graduate('Esq.');
+
+console.log(medicalSchool('Jacob Husband'));
+console.log(lawSchool('Jacob Husband'));
